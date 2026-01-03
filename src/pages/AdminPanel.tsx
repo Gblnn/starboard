eebfd69ff27316143d1a0ce40fea33daf296aeb5
Gokul <122973682+Gblnn@users.noreply.github.com>
2026-01-03 20:28:49 +0400

    test

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -37,7 +37,7 @@ export const AdminPanel = () => {
           />
 
         <div style={{border:"", display:"flex", width:"100%", justifyContent:"", alignItems:"flex-start", padding:"1rem", marginTop:"4rem", overflowY:"auto", flexFlow:"column", gap:"0.5rem"}}>
-            <Directive width={"100%"} icon={<UsersIcon width={"1rem"}/>} title={"User Management"}/>
+            <Directive width={"100%"} icon={<UsersIcon color='dodgerblue' width={"1rem"}/>} title={"User Management"}/>
             <Directive width={"100%"} icon={<LinkIcon width={"1rem"}/>} title={"Quick Links"}/>
             <Directive width={"100%"} icon={<QrCode width={"1rem"}/>} title={"QR Generator"}/>
         </div>
